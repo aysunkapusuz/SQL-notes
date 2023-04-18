ca2bbc6527a750ddf8be03a3e1fba46b2a4741ce
aysun <aysunkapusuz@gmail.com>
2023-04-18 07:56:12 -0600

    commenting

--- tablesAndKeys.sql
+++ tablesAndKeys.sql
@@ -1,7 +1,7 @@
-/* Primary key is unique. It can be string, number etc. A primary key is a set of one or more fields/columns of a table that uniquely identify a record in a database table.
-   Foreign key is way to define relationships between tables.
-   Super key is a set of one or more than one key that can be used to identify a record uniquely in a table. 
-   A Candidate Key is a set of one or more fields/columns that can identify a record uniquely in a table.
-   An Alternate key is a key that can work as a primary key. 
-   A composite Key is a combination of more than one field/column of a table. 
-   A unique key is a set of one or more fields/columns of a table that uniquely identify a record in a database table. */
+-- Primary key is unique. It can be string, number etc. A primary key is a set of one or more fields/columns of a table that uniquely identify a record in a database table.
+-- Foreign key is way to define relationships between tables.
+-- Super key is a set of one or more than one key that can be used to identify a record uniquely in a table. 
+-- A Candidate Key is a set of one or more fields/columns that can identify a record uniquely in a table.
+-- An Alternate key is a key that can work as a primary key. 
+-- A composite Key is a combination of more than one field/column of a table. 
+-- A unique key is a set of one or more fields/columns of a table that uniquely identify a record in a database table. 
